c314eaf001f3e7bf54897161f65fd7b90b2b69d1
jeremy776 <jeremy.kusuma2018@gmail.com>
2024-02-21 10:08:13 +0700

    up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,16 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
   },
-  plugins: [],
+  daisyui: {
+    themes: ["winter"],
+    base: true,
+    styled: true,
+    logs: true,
+    utils: true
+  },
+  plugins: [
+    require('daisyui')
+  ],
 };
 export default config;
